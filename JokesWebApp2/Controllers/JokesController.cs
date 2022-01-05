49b07a7fe389bf285c8c714a2173f19983df2b55
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2022-01-05 13:41:21 +0100

    added search function

--- JokesWebApp2/Controllers/JokesController.cs
+++ JokesWebApp2/Controllers/JokesController.cs
@@ -26,6 +26,18 @@ namespace JokesWebApp2.Controllers
             return View(await _context.Joke.ToListAsync());
         }
 
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View(); // empty view
+        }
+
+        // POST: Jokes/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Joke.Where( j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
